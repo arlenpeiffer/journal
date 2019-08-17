b75a330904ed398c2fc4ccb03f3be750c1dc7cdb
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-08-17 12:54:36 -0700

    adds firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB7oWFTr_VJa2xVIEj2WeN_tJ_F99ljYZM',
+  authDomain: 'journal-e4e37.firebaseapp.com',
+  databaseURL: 'https://journal-e4e37.firebaseio.com',
+  projectId: 'journal-e4e37',
+  storageBucket: '',
+  messagingSenderId: '424654276547',
+  appId: '1:424654276547:web:341cbf219110e359'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { firebase, database as default };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
+import './firebase/firebase';
 
 import App from './components/App';
 

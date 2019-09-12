857919e6b41d93d4b532d989905134924e83506a
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-09-11 21:20:31 -0700

    adds separate database for test environment

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-const firebaseConfig = {
+const standardConfig = {
   apiKey: 'AIzaSyB7oWFTr_VJa2xVIEj2WeN_tJ_F99ljYZM',
   authDomain: 'journal-e4e37.firebaseapp.com',
   databaseURL: 'https://journal-e4e37.firebaseio.com',
@@ -11,6 +11,19 @@ const firebaseConfig = {
   messagingSenderId: '424654276547',
   appId: '1:424654276547:web:341cbf219110e359'
 };
+
+const testConfig = {
+  apiKey: 'AIzaSyBbCyR0Mul1w-s9zM35kz93ft1Fch3EklM',
+  authDomain: 'journal-test-da59a.firebaseapp.com',
+  databaseURL: 'https://journal-test-da59a.firebaseio.com',
+  projectId: 'journal-test-da59a',
+  storageBucket: '',
+  messagingSenderId: '820529092236',
+  appId: '1:820529092236:web:9b17b1266ad5af71b9cda9'
+};
+
+const firebaseConfig =
+  process.env.NODE_ENV === 'test' ? testConfig : standardConfig;
 
 firebase.initializeApp(firebaseConfig);
 

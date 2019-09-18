4e4b3569c98a08927cd7ce929fd58380ac15e136
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-09-18 15:38:08 -0700

    refactors action types

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,21 +1,18 @@
-// userInfo
-export const SET_IS_LOGGED_IN = 'SET_IS_LOGGED_IN';
-export const SET_IS_LOGGED_OUT = 'SET_IS_LOGGED_OUT';
-
+// filters
+export const RESET_FILTERS = 'RESET_FILTERS';
+export const SET_FILTERS = 'SET_FILTERS';
 // journal
 export const ADD_ENTRY = 'ADD_ENTRY';
 export const EDIT_ENTRY = 'EDIT_ENTRY';
 export const GET_JOURNAL = 'GET_JOURNAL';
 export const REMOVE_ENTRY = 'REMOVE_ENTRY';
-
-// log
+// logs
 export const ADD_FOOD = 'ADD_FOOD';
 export const ADD_MOVEMENT = 'ADD_MOVEMENT';
 export const ADD_NSAID = 'ADD_NSAID';
 export const ADD_SUPPLEMENT = 'ADD_SUPPLEMENT';
 export const GET_LOGS = 'GET_LOGS';
 export const REMOVE_SUPPLEMENT = 'REMOVE_SUPPLEMENT';
-
-// filters
-export const RESET_FILTERS = 'RESET_FILTERS';
-export const SET_FILTERS = 'SET_FILTERS';
+// userInfo
+export const SET_IS_LOGGED_IN = 'SET_IS_LOGGED_IN';
+export const SET_IS_LOGGED_OUT = 'SET_IS_LOGGED_OUT';

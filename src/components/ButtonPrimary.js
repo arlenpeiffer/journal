78cb7046677d0a10cff84848275ac61ba7bb979f
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-12-16 11:39:11 -0800

    Adds ButtonPrimary component

---
+++ src/components/ButtonPrimary.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const ButtonPrimary = props => (
+  <Button color="primary" variant="contained" {...props} />
+);
+
+export default ButtonPrimary;

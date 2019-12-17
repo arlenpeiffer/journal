5a4c371dbb298b3daa82df8d40fb811b4024db2b
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-12-17 13:48:26 -0800

    Updates styles for ButtonPrimary

--- src/components/ButtonPrimary.js
+++ src/components/ButtonPrimary.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const ButtonPrimary = props => (
-  <Button color="primary" variant="contained" {...props} />
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(1)
+  }
+}));
+
+const ButtonPrimary = props => {
+  const classes = useStyles();
+
+  return (
+    <Button
+      className={classes.root}
+      color="primary"
+      variant="contained"
+      {...props}
+    />
+  );
+};
 
 export default ButtonPrimary;

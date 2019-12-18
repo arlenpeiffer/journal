56d1f51958978430d711363307dfeebcdd5fc5b5
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-12-17 16:53:40 -0800

    Removes Loading component

--- src/components/ViewEntries.js
+++ src/components/ViewEntries.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import Entries from './Entries';
 import Filters from './Filters';
-import Loading from './Loading';
 
-function ViewEntries(props) {
-  const { requests } = props;
-
-  return (
-    <div>
-      ViewEntries.js
-      <Filters />
-      {requests > 0 ? <Loading tip="Loading User Data..." /> : <Entries />}
-    </div>
-  );
-}
+const ViewEntries = ({ requests }) => (
+  <div>
+    <Filters />
+    {requests > 0 ? <CircularProgress /> : <Entries />}
+  </div>
+);
 
 const mapStateToProps = state => ({
   requests: state.ui.requests

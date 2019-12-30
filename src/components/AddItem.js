80bd2674542eba3fdfb6289d56a90a78ed458a9f
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-12-29 22:24:28 -0800

    WIP setting up AddItem component

---
+++ src/components/AddItem.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import Icon from '@material-ui/core/Icon';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Typography from '@material-ui/core/Typography';
+
+import ButtonPrimary from './ButtonPrimary';
+
+const useStyles = makeStyles(theme => ({
+  inputInner: {
+    paddingTop: '9px',
+    paddingBottom: '9px'
+  },
+  inputOuter: {
+    marginRight: theme.spacing(1)
+  },
+  label: {
+    alignItems: 'center',
+    cursor: 'pointer',
+    display: 'inline-flex',
+    '& > :first-child': {
+      marginRight: theme.spacing(0.5)
+    }
+  },
+  row: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  wrapper: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  }
+}));
+
+const AddItem = ({ dataSource, onClick, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  const classes = useStyles();
+
+  const icon = isOpen ? 'expand_more' : 'add';
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    const valueExists = dataSource.some(
+      item => item.toLowerCase() === value.toLowerCase()
+    );
+
+    if (!valueExists) {
+      onClick(value);
+      setValue('');
+    }
+    // display error message
+    // reset value
+  };
+
+  const toggleExpanded = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <span className={classes.label} onClick={toggleExpanded}>
+        <Icon fontSize="small">{icon}</Icon>
+        <Typography variant="button">Add new</Typography>
+      </span>
+      <Collapse in={isOpen} timeout={150}>
+        <div className={classes.row}>
+          <OutlinedInput
+            classes={{ input: classes.inputInner }}
+            className={classes.inputOuter}
+            onChange={handleChange}
+            value={value}
+            variant="outlined"
+          />
+          <ButtonPrimary onClick={handleClick}>Add</ButtonPrimary>
+        </div>
+      </Collapse>
+    </div>
+  );
+};
+
+export default AddItem;

6da80fc996ea48260b60bcbda696a557a75d0a6a
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-01-08 14:21:37 -0800

    Refactors Select to work for all use cases

--- src/components/Select.js
+++ src/components/Select.js
@@ -7,6 +7,9 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import MuiSelect from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+
+import AddItem from './AddItem';
 
 const useStyles = makeStyles(theme => ({
   divider: {
@@ -14,13 +17,21 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1.5),
     marginBottom: theme.spacing(0.5),
     marginLeft: theme.spacing(1.5)
+  },
+  emptyPrompt: {
+    paddingTop: theme.spacing(0.75),
+    paddingBottom: theme.spacing(0.75),
+    paddingLeft: theme.spacing(2)
   }
 }));
 
 const Select = ({
+  addItemCallback,
   children,
   dataSource,
-  defaultOption,
+  emptyPrompt,
+  formatValue,
+  includeAddItem,
   label,
   name,
   ...props
@@ -32,32 +43,63 @@ const Select = ({
 
   const classes = useStyles();
 
+  const handleRenderValue = value => {
+    const valueIsEmpty = value === '' || value === undefined;
+
+    if (valueIsEmpty)
+      return (
+        <Typography color="textSecondary">
+          <em>Select an option</em>
+        </Typography>
+      );
+
+    if (formatValue) return formatValue(value);
+
+    return value;
+  };
+
+  const footer = [
+    <Divider className={classes.divider} key="divider" />,
+    <AddItem
+      callback={addItemCallback}
+      dataSource={dataSource}
+      key="addItem"
+      padSides
+    />
+  ];
+
+  const menuItemsFromDataSource =
+    dataSource && dataSource.length > 0 ? (
+      dataSource.map(item => (
+        <MenuItem key={item} value={item}>
+          {item}
+        </MenuItem>
+      ))
+    ) : (
+      <Typography
+        className={classes.emptyPrompt}
+        color="textSecondary"
+        variant="body2"
+      >
+        <em>{emptyPrompt}</em>
+      </Typography>
+    );
+
   return (
     <FormControl error={hasError}>
-      <InputLabel>{label}</InputLabel>
+      <InputLabel shrink>{label}</InputLabel>
       <MuiSelect
+        displayEmpty
         name={name}
         onBlur={field.onBlur}
         onChange={field.onChange}
+        renderValue={handleRenderValue}
         value={field.value}
         {...props}
       >
-        {defaultOption && (
-          <MenuItem value={defaultOption}>
-            <em>{defaultOption}</em>
-          </MenuItem>
-        )}
-        {dataSource.map(item => (
-          <MenuItem key={item} value={item}>
-            {item}
-          </MenuItem>
-        ))}
-        {children && (
-          <>
-            <Divider className={classes.divider} />
-            {children}
-          </>
-        )}
+        {children}
+        {dataSource && menuItemsFromDataSource}
+        {includeAddItem && footer.map(item => item)}
       </MuiSelect>
       {hasError && <FormHelperText>{error}</FormHelperText>}
     </FormControl>

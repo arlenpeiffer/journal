f52f9fde485ae5ef0943429c5276c4932e0a01df
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-02-19 23:37:27 -0800

    WIP refactoring utils/logs

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -1,66 +1,70 @@
 import * as actions from '../redux/actions/logs';
 import store from '../redux/store';
 
-// ----- LOGS ----- //
 export const checkIfLogContainsValue = (log, value) => {
   return log.some(logItem => logItem.toLowerCase() === value.toLowerCase());
 };
 
-export const determineValuesToLog = (entrySection, sectionProperty, log) => {
-  const valuesToLog = [];
-  entrySection.map(sectionItem => {
-    const value = sectionProperty ? sectionItem[sectionProperty] : sectionItem;
-    const logContainsValue = checkIfLogContainsValue(log, value);
-    return logContainsValue ? null : valuesToLog.push(value);
-  });
-  return valuesToLog;
+const gather = (array, properties) => {
+  const values = array.reduce((acc, item) => {
+    const property = properties[0];
+    const value = item[property];
+    const isArray = Array.isArray(value);
+    isArray ? acc.push(...value) : acc.push(value);
+    return acc;
+  }, []);
+
+  const isLastProperty = properties.length === 1;
+  return isLastProperty ? values : gather(values, properties.slice(1));
 };
 
-export const handleLogValues = (values, thunk) => {
+const filter = (values, log) => {
+  return values.filter(value => !checkIfLogContainsValue(log, value));
+};
+
+const updateAppointments = (appointments, log) => {
+  const valuesInEntry = gather(appointments, ['type']);
+  const valuesToLog = filter(valuesInEntry, log);
+  updateLog(valuesToLog, actions.addAppointment);
+};
+
+const updateIngredients = (meals, log) => {
+  const ingredientsInEntry = gather(meals, ['items', 'ingredients']);
+  const ingredientsToLog = filter(ingredientsInEntry, log);
+  updateLog(ingredientsToLog, actions.addIngredient);
+};
+
+const updateMealItems = (meals, log) => {
+  const mealItemsInEntry = gather(meals, ['items', 'name']);
+  const mealItemsToLog = filter(mealItemsInEntry, log);
+  updateLog(mealItemsToLog, actions.addMeal);
+};
+
+const updateMovements = (movements, log) => {
+  const movementsInEntry = gather(movements, ['type']);
+  const movementsToLog = filter(movementsInEntry, log);
+  updateLog(movementsToLog, actions.addMovement);
+};
+
+const updatePractitioners = (appointments, log) => {
+  const practitionersInEntry = gather(appointments, ['practitioner']);
+  const practitionersToLog = filter(practitionersInEntry, log);
+  updateLog(practitionersToLog, actions.addPractitioner);
+};
+
+const updateLog = (valuesToLog, action) => {
   const { dispatch } = store;
-  const shouldUpdate = values.length > 0;
-  shouldUpdate && values.map(value => dispatch(thunk(value)));
+  const dispatchActionForEachValue = valuesToLog.map(value =>
+    dispatch(action(value))
+  );
+  return valuesToLog && dispatchActionForEachValue;
 };
 
-export const updateLogs = (entry, logs) => {
-  const appointments = determineValuesToLog(
-    entry.appointments,
-    'type',
-    logs.appointments
-  );
-  handleLogValues(appointments, actions.addAppointment);
-
-  // const food = [];
-  // entry.food.meals.map(meal => {
-  //   const items = determineValuesToLog(meal.items, 'name', logs.food);
-  //   food.push(...items);
-  // });
-  // logValues(food, actions.addFood);
-
-  const ingredients = [];
-  entry.food.meals.map(meal => {
-    meal.items.map(item => {
-      ingredients.push(
-        ...determineValuesToLog(item.ingredients, null, logs.ingredients)
-      );
-    });
-  });
-  handleLogValues(ingredients, actions.addIngredient);
-
-  const meals = [];
-  entry.food.meals.map(meal => {
-    const items = determineValuesToLog(meal.items, 'name', logs.food);
-    meals.push(...items);
-  });
-  handleLogValues(meals, actions.addMeal);
-
-  const movement = determineValuesToLog(entry.movement, 'type', logs.movement);
-  handleLogValues(movement, actions.addMovement);
-
-  const practitioners = determineValuesToLog(
-    entry.appointments,
-    'practitioner',
-    logs.practitioners
-  );
-  handleLogValues(practitioners, actions.addPractitioner);
+export const updateLogs = entry => {
+  const { logs } = store.getState().user;
+  updateAppointments(entry.appointments, logs.appointments);
+  updateIngredients(entry.food.meals, logs.ingredients);
+  updateMealItems(entry.food.meals, logs.meals);
+  updateMovements(entry.movement, logs.movement);
+  updatePractitioners(entry.appointments, logs.practitioners);
 };
